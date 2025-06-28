82e55d407507e7ba19282cd1dddf1ef030dc1e2a
Adetola Adedoyin <108130465+Adetola-Adedoyin@users.noreply.github.com>
2025-06-28 12:47:15 +0100

    Update s3-website.tf

--- s3-website.tf
+++ s3-website.tf
@@ -224,3 +224,27 @@ output "cloudfront_domain_name" {
   value       = aws_cloudfront_distribution.website_distribution.domain_name
   description = "Domain name of the CloudFront distribution (with HTTPS)"
 }
+resource "aws_s3_bucket_public_access_block" "disable_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.disable_block]
+}
